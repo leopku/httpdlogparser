dc1dc97141c8bbedc96b7b1e58c12a247efc5ac6
leopku <leopku@qq.com>
2009-12-07 16:24:38 +0800

    extend ofc_flash_chart class to extChart.
    Signed-off-by: leopku <leopku@qq.com>

--- extofc.py
+++ extofc.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-import ofc2_element
 from ofc2 import open_flash_chart
 
 class extChart(open_flash_chart):
-    def __init__(self, title, style=None):
-        open_flash_chart.__init__(self, title, style)
+    #def __init__(self, title, style=None):
+    #    open_flash_chart.__init__(title, style)
     
     def add_grid_line(self, line):
         try:
             self['rows'].append(line)
         except:
-            self['rows'] = [line]    
+            self['rows'] = [line]
